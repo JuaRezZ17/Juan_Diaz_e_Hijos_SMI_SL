a336233a97f3b438a17ec987c01452bcf9a94895
JuaRezZ17 <juanra97sg@gmail.com>
2025-05-18 12:31:41 +0100

    18-05-2025

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ const menu = document.querySelector('.menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('mostrar');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elementos = document.querySelectorAll('.animada');
+
+  const observer = new IntersectionObserver((entradas, obs) => {
+    entradas.forEach(entrada => {
+      if (entrada.isIntersecting) {
+        entrada.target.classList.add('visible');
+        obs.unobserve(entrada.target); // solo una vez
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  elementos.forEach(el => observer.observe(el));
+});
